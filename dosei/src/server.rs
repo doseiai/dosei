86781027938aced54d3ec6f72fd0db222338f8bf
Alw3ys <alw3ys@gmail.com>
2023-12-08 14:52:44 +0100

    routing

--- dosei/src/server.rs
+++ dosei/src/server.rs
@@ -1,14 +1,14 @@
 mod cluster;
 mod cron;
 
-use axum::{routing::get, Router};
+use axum::{routing, Router};
 use log::{info};
 use crate::config::{Config};
 
 pub async fn start_server(config: &Config) {
   cluster::start_main_node(config);
   cron::start_job_manager();
-  let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+  let app = Router::new().route("/", routing::get(|| async { "Hello, World!" }));
   let address = config.address.to_string();
   info!("Dosei running on http://{} (Press CTRL+C to quit", address);
   axum::Server::bind(&address.parse().unwrap()).serve(app.into_make_service()).await.unwrap();
