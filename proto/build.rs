4f556ac02f215d13a3882762b47031f1915eb413
Alw3ys <alw3ys@gmail.com>
2023-12-09 20:42:05 +0100

    idk

--- proto/build.rs
+++ proto/build.rs
@@ -1,6 +1,6 @@
 use std::io::Result;
 
 fn main() -> Result<()> {
-    prost_build::compile_protos(&["src/cron_job.proto", "src/cluster.proto"], &["src/"])?;
+    prost_build::compile_protos(&["src/cluster.proto", "src/cron_job.proto"], &["src/"])?;
     Ok(())
 }
