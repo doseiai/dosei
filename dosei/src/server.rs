7466bf596d7187898eabe9e9451d753e18150704
Alw3ys <alw3ys@gmail.com>
2023-12-10 18:59:37 +0100

    few fixes

--- dosei/src/server.rs
+++ dosei/src/server.rs
@@ -5,24 +5,17 @@ use std::env;
 use sqlx::Pool;
 use sqlx::postgres::Postgres;
 
-use axum::{routing, Router, Json};
+use axum::{routing, Router};
 use log::{info};
 use crate::{schema};
 use crate::config::{Config};
-use crate::schema::CronJob;
-
-async fn get_cron_jobs(pool: Pool<Postgres>) -> Json<Vec<CronJob>> {
-  let recs = sqlx::query_as!(schema::CronJob, "SELECT * from cron_jobs")
-    .fetch_all(&pool).await.unwrap();
-  Json(recs)
-}
 
 pub async fn start_server(config: &Config) {
-  cluster::start_node(config);
-  cron::start_job_manager(config);
   let pool = Pool::<Postgres>::connect(&env::var("DATABASE_URL").unwrap()).await.unwrap();
+  cluster::start_node(config);
+  cron::start_job_manager(config, pool.clone());
   let cron_job = schema::cron_job_mock();
-  let rec = sqlx::query!(
+  sqlx::query!(
     r#"
     INSERT INTO cron_jobs (uuid, schedule, entrypoint, owner_id, deployment_id, updated_at, created_at)
     VALUES ($1, $2, $3, $4, $5, $6, $7)
@@ -37,7 +30,7 @@ pub async fn start_server(config: &Config) {
     cron_job.created_at
   ).fetch_one(&pool).await.unwrap();
   info!("Successfully connected to Postgres");
-  let app = Router::new().route("/", routing::get(move || get_cron_jobs(pool.clone())));
+  let app = Router::new().route("/", routing::get(move || cron::get_cron_jobs(pool.clone())));
   let address = config.address.to_string();
   info!("Dosei running on http://{} (Press CTRL+C to quit", address);
   axum::Server::bind(&address.parse().unwrap()).serve(app.into_make_service()).await.unwrap();
