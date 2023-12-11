1d66b1503681c14ab0979ad968a51d3c8e040201
rahul <rahulkp220@gmail.com>
2023-12-12 02:53:42 +0530

    feat: fix linter to use 2 spaces

--- dosei/src/main.rs
+++ dosei/src/main.rs
@@ -6,9 +6,9 @@ use dotenv::dotenv;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    dotenv().ok();
-    env_logger::init();
-    let config: Config = config::init();
-    server::start_server(&config).await;
-    Ok(())
+  dotenv().ok();
+  env_logger::init();
+  let config: Config = config::init();
+  server::start_server(&config).await;
+  Ok(())
 }
