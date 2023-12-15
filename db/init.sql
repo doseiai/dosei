31d788423455ad51940d74bcbebc4e15bce6c722
Alw3ys <alw3ys@gmail.com>
2023-12-15 12:55:21 +0100

    add env table

--- db/init.sql
+++ db/init.sql
@@ -9,3 +9,15 @@ CREATE TABLE cron_jobs (
    PRIMARY KEY (uuid),
    UNIQUE (uuid, owner_id, deployment_id)
 );
+
+CREATE TABLE envs (
+   uuid UUID NOT NULL,
+   name TEXT NOT NULL,
+   value integer[] NOT NULL,
+   project_id UUID NOT NULL,
+   owner_id UUID NOT NULL,
+   updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+   created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+   PRIMARY KEY (uuid),
+   UNIQUE (name, project_id, owner_id)
+);
