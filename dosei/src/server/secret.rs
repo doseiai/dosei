5aa2c7cb74abeb51f1106b57db4d2c96c41831d5
rahul <rahulkp220@gmail.com>
2023-12-17 14:50:53 +0530

    feat: initial commit for setting env vars

--- dosei/src/server/secret.rs
+++ dosei/src/server/secret.rs
@@ -5,6 +5,7 @@ use serde::Deserialize;
 use sqlx::{Pool, Postgres};
 use std::collections::HashMap;
 use std::sync::Arc;
+use uuid::Uuid;
 
 pub async fn api_get_envs(pool: Extension<Arc<Pool<Postgres>>>) -> Json<Vec<Secret>> {
   let recs = sqlx::query_as!(Secret, "SELECT * from envs")
@@ -18,16 +19,48 @@ pub async fn api_set_envs(
   pool: Extension<Arc<Pool<Postgres>>>,
   Query(query): Query<SetEnvsQueryParams>,
   Json(body): Json<HashMap<String, String>>,
-) -> Json<HashMap<String, String>> {
+) -> Json<Vec<Secret>> {
   println!("{:?}", body);
   println!("{query:?}");
-  let rec = HashMap::new();
-  Json(rec)
+  for (key, value) in body.into_iter() {
+    let secret = Secret {
+      id: Uuid::new_v4(),
+      name: key,
+      value: value,
+      owner_id: query.owner_name,
+      project_id: query.project_name,
+      updated_at: Default::default(),
+      created_at: Default::default(),
+    };
+    let _rec = sqlx::query_as!(
+      Secret,
+      r#"
+      INSERT INTO envs (id, name, value, owner_id, project_id, updated_at, created_at)
+      VALUES ($1, $2, $3, $4, $5, $6, $7)
+      RETURNING *
+      "#,
+      secret.id,
+      secret.name,
+      secret.value,
+      secret.owner_id,
+      secret.project_id,
+      secret.updated_at,
+      secret.created_at
+    )
+    .fetch_one(&**pool)
+    .await
+    .unwrap();
+  }
+  let recs = sqlx::query_as!(Secret, "SELECT * from envs")
+    .fetch_all(&**pool)
+    .await
+    .unwrap();
+  Json(recs)
 }
 
 #[allow(dead_code)]
 #[derive(Deserialize, Debug)]
 pub struct SetEnvsQueryParams {
-  owner_name: Option<String>,
-  project_name: Option<String>,
+  owner_name: Uuid,
+  project_name: Uuid,
 }
