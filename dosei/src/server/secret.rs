7f932f5a0b60e434e53897bca2589208649650f9
Alw3ys <alw3ys@gmail.com>
2023-12-17 18:44:32 +0100

    change query

--- dosei/src/server/secret.rs
+++ dosei/src/server/secret.rs
@@ -4,10 +4,11 @@ use axum::http::StatusCode;
 use axum::{Extension, Json};
 use log::error;
 use serde::Deserialize;
-use sqlx::{Pool, Postgres, QueryBuilder};
+use sqlx::{Pool, Postgres};
 use std::collections::HashMap;
 use std::sync::Arc;
 use uuid::Uuid;
+use chrono::Utc;
 
 pub async fn api_get_envs(
   pool: Extension<Arc<Pool<Postgres>>>,
@@ -37,7 +38,7 @@ pub async fn api_get_envs(
   match result {
     Ok(recs) => Ok(Json(recs)),
     Err(err) => {
-      error!("{:?}", err);
+      error!("Error in retrieving secret: {:?}", err);
       Err(StatusCode::INTERNAL_SERVER_ERROR)
     }
   }
@@ -47,78 +48,34 @@ pub async fn api_set_envs(
   pool: Extension<Arc<Pool<Postgres>>>,
   Path(params): Path<EnvsPathParams>,
   Json(body): Json<HashMap<String, String>>,
-) -> Json<Vec<Secret>> {
-  let mut secrets: Vec<Secret> = vec![];
+) -> Result<Json<Vec<Secret>>, StatusCode> {
+  let mut updated_secrets: Vec<Secret> = Vec::new();
 
-  for (name, value) in body.into_iter() {
-    // check db for this name and see if it exists
-    let recs = sqlx::query_as!(
+  for (name, value) in body {
+    let query = sqlx::query_as!(
       Secret,
-      r#"SELECT * FROM envs WHERE owner_id = $1::uuid and name = $2::text and value = $3::text"#,
-      params.owner_id,
+      "INSERT INTO envs (id, name, value, owner_id, project_id, updated_at, created_at) VALUES ($1, $2, $3, $4, $5, $6, $7)
+       ON CONFLICT (owner_id, project_id, name) DO UPDATE
+       SET value = EXCLUDED.value, updated_at = EXCLUDED.updated_at
+       RETURNING *",
+      Uuid::new_v4(),
       name,
-      value
-    )
-    .fetch_all(&**pool)
-    .await
-    .unwrap();
+      value,
+      params.owner_id,
+      params.project_id,
+      Utc::now(),
+      Utc::now()
+    );
 
-    // if nothing, set for update
-    if recs.is_empty() {
-      secrets.push(Secret {
-        id: Uuid::new_v4(),
-        name,
-        value,
-        owner_id: params.owner_id,
-        project_id: params.project_id,
-        updated_at: Default::default(),
-        created_at: Default::default(),
-      });
+    match query.fetch_one(&**pool).await {
+      Ok(secret) => updated_secrets.push(secret),
+      Err(err) => {
+        error!("Error in upserting and retrieving secret: {:?}", err);
+        return Err(StatusCode::INTERNAL_SERVER_ERROR);
+      }
     }
   }
-
-  let mut query_builder = QueryBuilder::new(
-    "INSERT INTO envs (id, name, value, owner_id, project_id, updated_at, created_at) ",
-  );
-  query_builder.push_values(secrets, |mut qb, scr| {
-    qb.push_bind(scr.id)
-      .push_bind(scr.name)
-      .push_bind(scr.value)
-      .push_bind(scr.owner_id)
-      .push_bind(scr.project_id)
-      .push_bind(scr.updated_at)
-      .push_bind(scr.created_at);
-  });
-
-  query_builder.build().execute(&**pool).await.unwrap();
-
-  // todo
-  // ideally should get just the list of env vars updated via the call
-  match params.project_id {
-    Some(_) => {
-      let recs = sqlx::query_as!(
-        Secret,
-        r#"SELECT * FROM envs WHERE project_id = $1::uuid and owner_id = $2::uuid"#,
-        params.project_id,
-        params.owner_id
-      )
-      .fetch_all(&**pool)
-      .await
-      .unwrap();
-      Json(recs)
-    }
-    None => {
-      let recs = sqlx::query_as!(
-        Secret,
-        r#"SELECT * FROM envs WHERE owner_id = $1::uuid"#,
-        params.owner_id
-      )
-      .fetch_all(&**pool)
-      .await
-      .unwrap();
-      Json(recs)
-    }
-  }
+  Ok(Json(updated_secrets))
 }
 
 #[derive(Deserialize, Debug)]
