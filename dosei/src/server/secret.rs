184bc2ffe33b2f0740a28bd71a1b5c247a4dec1f
rahul <rahulkp220@gmail.com>
2023-12-17 16:06:54 +0530

    feat: implement querybuilder

--- dosei/src/server/secret.rs
+++ dosei/src/server/secret.rs
@@ -2,7 +2,7 @@ use crate::schema::Secret;
 use axum::extract::Query;
 use axum::{Extension, Json};
 use serde::Deserialize;
-use sqlx::{Pool, Postgres};
+use sqlx::{Pool, Postgres, QueryBuilder};
 use std::collections::HashMap;
 use std::sync::Arc;
 use uuid::Uuid;
@@ -21,8 +21,9 @@ pub async fn api_set_envs(
   Json(body): Json<HashMap<String, String>>,
 ) -> Json<Vec<Secret>> {
   let mut secrets: Vec<Secret> = vec![];
+
   for (name, value) in body.into_iter() {
-    let secret = Secret {
+    secrets.push(Secret {
       id: Uuid::new_v4(),
       name,
       value,
@@ -30,29 +31,31 @@ pub async fn api_set_envs(
       project_id: query.project_name,
       updated_at: Default::default(),
       created_at: Default::default(),
-    };
-    let rec = sqlx::query_as!(
-      Secret,
-      r#"
-      INSERT INTO envs (id, name, value, owner_id, project_id, updated_at, created_at)
-      VALUES ($1, $2, $3, $4, $5, $6, $7)
-      RETURNING *
-      "#,
-      secret.id,
-      secret.name,
-      secret.value,
-      secret.owner_id,
-      secret.project_id,
-      secret.updated_at,
-      secret.created_at
-    )
-    .fetch_one(&**pool)
+    });
+  }
+
+  let mut query_builder = QueryBuilder::new(
+    "INSERT INTO envs (id, name, value, owner_id, project_id, updated_at, created_at) ",
+  );
+  query_builder.push_values(secrets, |mut b, new_secret| {
+    b.push_bind(new_secret.id)
+      .push_bind(new_secret.name)
+      .push_bind(new_secret.value)
+      .push_bind(new_secret.owner_id)
+      .push_bind(new_secret.project_id)
+      .push_bind(new_secret.updated_at)
+      .push_bind(new_secret.created_at);
+  });
+
+  let query = query_builder.build();
+  query.execute(&**pool).await.unwrap();
+
+  let recs = sqlx::query_as!(Secret, "SELECT * from envs")
+    .fetch_all(&**pool)
     .await
     .unwrap();
 
-    secrets.push(rec);
-  }
-  Json(secrets)
+  Json(recs)
 }
 
 #[allow(dead_code)]
