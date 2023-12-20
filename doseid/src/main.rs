98a88ccff8c879bf27f2ec81dfdea747e743dedc
Alw3ys <alw3ys@gmail.com>
2023-12-21 00:07:42 +0100

    remove printf

--- doseid/src/main.rs
+++ doseid/src/main.rs
@@ -7,7 +7,6 @@ use config::Config;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
   let config: &'static Config = Box::leak(Box::new(config::init()?));
-  println!("{}", config.telemetry_disabled);
   server::start_server(config).await?;
   Ok(())
 }
