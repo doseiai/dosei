588f6153bdffaaa55f55067accaf822d44feae12
Alw3ys <alw3ys@gmail.com>
2023-12-21 12:40:28 +0100

    identify

--- doseid/src/main.rs
+++ doseid/src/main.rs
@@ -7,6 +7,9 @@ use config::Config;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
   let config: &'static Config = Box::leak(Box::new(config::init()?));
+  if !config.telemetry.is_disabled() {
+    config.telemetry.client.as_ref().unwrap().identify().await;
+  }
   server::start_server(config).await?;
   Ok(())
 }
