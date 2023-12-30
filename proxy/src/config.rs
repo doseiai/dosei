0a92aedce09a6fdd7a37ea29c79e84b124204cf7
Rahul <rahulkp220@users.noreply.github.com>
2023-12-30 17:04:27 +0530

    Add missing logger for proxy (#28)
    * fix: add missing logger
    
    * fix: move logger to config
    
    * fix: drop comment

--- proxy/src/config.rs
+++ proxy/src/config.rs
@@ -30,6 +30,13 @@ impl Config {
     if env::var("RUST_LOG").is_err() {
       env::set_var("RUST_LOG", "info");
     }
+
+    let subscriber = tracing_subscriber::fmt()
+      .with_line_number(true)
+      .with_target(true)
+      .finish();
+    tracing::subscriber::set_global_default(subscriber)?;
+
     let redis_url = match env::var("REDIS_URL") {
       Ok(_url) => {
         warn!("TODO: Implement redis, falling back to single instance caching.");
