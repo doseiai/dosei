20919067befc6b6ac87ae4a90ad81ac4e2294ee7
Alw3ys <alw3ys@gmail.com>
2023-12-31 21:57:27 +0100

    keep track

--- doseid/src/deployment.rs
+++ doseid/src/deployment.rs
@@ -1,19 +1,43 @@
-use crate::docker::{build_image, push_image};
+use crate::docker::{build_image, push_image, run_command};
 use std::path::Path;
-use tracing::info;
+use tracing::error;
+use dosei_util::Framework;
+
+
+
+// This is some code I've on python which I need to move here
+// app_json = DockerManager.run_command(
+// tag,
+// f'sh -c "dosei export {app_instance} && cat .dosei/app.json"'
+// )
+// dosei_app = Dosei(**json.loads(app_json))
+// cron_jobs = dosei_app.cron_jobs
 
 async fn build(folder_path: &Path) {
   let detected_docker_file = dosei_util::package_manager::_resolve_docker(folder_path);
   if detected_docker_file {
     println!("Detected `Dockerfile`");
     let _ = build_image("example", "example", folder_path).await;
-    let _ = push_image("example", "example").await;
+
+    match dosei_util::_find_framework_init(&Framework::Dosei, folder_path) {
+      Ok(entrypoint) => {
+        println!("{}", entrypoint);
+        let _ = run_command("example", "example", entrypoint.as_str()).await;
+      }
+      Err(_) => {
+        eprintln!("We couldn't find it!");
+        return error!("We couldn't find it")
+      }
+    }
+
+    // let _ = push_image("example", "example").await;
   }
 }
 
+
 #[cfg(test)]
 mod tests {
-  use crate::deployment::build;
+  use crate::deployment::{build};
   use crate::git::git_clone;
   use git2::Repository;
   use tempfile::tempdir;
