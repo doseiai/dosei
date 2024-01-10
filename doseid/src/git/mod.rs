b6977e9a7b2eac4d0f67c63bb2424e2704588d12
Alw3ys <alw3ys@gmail.com>
2024-01-10 15:12:04 +0100

    feat: git push

--- doseid/src/git/mod.rs
+++ doseid/src/git/mod.rs
@@ -1,6 +1,6 @@
 pub(crate) mod github;
 use git2::build::RepoBuilder;
-use git2::{FetchOptions, Repository};
+use git2::{FetchOptions, IndexAddOption, Repository, Signature};
 use regex::Regex;
 use std::path::Path;
 use tokio::task;
@@ -38,6 +38,36 @@ pub async fn git_clone(
       }
       Err(e) => panic!("failed to clone: {}", e),
     }
+  })
+  .await?
+}
+
+pub async fn git_push(
+  from_url: &str,
+  from_path: &Path,
+  name: &str,
+  email: &str,
+) -> anyhow::Result<()> {
+  let from_url = from_url.to_string();
+  let from_path = from_path.to_path_buf();
+  let name = name.to_string();
+  let email = email.to_string();
+  task::spawn_blocking(move || {
+    let repo = Repository::init(from_path)?;
+
+    let sig = Signature::now(&name, &email)?;
+
+    let mut index = repo.index()?;
+    index.add_all(["*"].iter(), IndexAddOption::DEFAULT, None)?;
+    index.write()?;
+    let oid = index.write_tree()?;
+
+    let tree = repo.find_tree(oid)?;
+    repo.commit(Some("HEAD"), &sig, &sig, "Initial commit", &tree, &[])?;
+
+    let mut remote = repo.remote("origin", &from_url)?;
+    remote.push(&["refs/heads/main:refs/heads/main"], None)?;
+    Ok(())
   })
   .await?
 }
