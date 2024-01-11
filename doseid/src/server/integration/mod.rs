44b33b0ad6c7bcd7b24b5f5d366f2374a75422c5
Alw3ys <alw3ys@gmail.com>
2024-01-11 12:26:14 +0100

    design concept git integration trait

--- doseid/src/server/integration/mod.rs
+++ doseid/src/server/integration/mod.rs
@@ -1,1 +1,12 @@
+use git2::Repository;
+use std::path::Path;
 pub(crate) mod github;
+
+// TODO: Design concept of Git Source Integration trait
+pub trait GitSource {
+  fn new() -> anyhow::Result<()>;
+
+  fn git_pull(from_url: &str, to_path: &Path, branch: Option<&str>) -> anyhow::Result<Repository>;
+
+  fn git_push(from_url: &str, from_path: &Path, name: &str, email: &str) -> anyhow::Result<()>;
+}
