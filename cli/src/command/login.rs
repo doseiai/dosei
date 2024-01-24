f36a31011c42cada97068b64484ae555983c0d41
Alw3ys <alw3ys@gmail.com>
2024-01-24 13:33:25 +0100

    feat: implement login

--- cli/src/command/login.rs
+++ cli/src/command/login.rs
@@ -1,5 +1,54 @@
-use crate::config::{Config, GITHUB_CLIENT_ID};
+use crate::config::{Config, SessionCredentials, GITHUB_CLIENT_ID};
+use reqwest::Url;
+use tiny_http::{Header, Response, Server};
 
 pub fn login(config: &'static Config) {
-  let auth_url = format!("https://github.com/login/oauth/authorize?client_id={}&redirect_uri=http://localhost:8085/auth/github/cli&scope=read:user,user:email", GITHUB_CLIENT_ID);
+  let host = "localhost:8085";
+  let base_url = format!("http://{}", host);
+  let server = Server::http(host).unwrap();
+  let auth_url = format!(
+    "https://github.com/login/oauth/authorize?client_id={}&redirect_uri={}/auth/github/cli&scope=read:user,user:email",
+    GITHUB_CLIENT_ID,
+    base_url
+  );
+
+  if webbrowser::open(&auth_url).is_ok() {
+    println!("Your browser has been opened to visit:\n\n{}\n", auth_url);
+  }
+  if let Some(request) = server.incoming_requests().next() {
+    if request.url().starts_with("/auth/github/cli") {
+      let url = Url::parse(&format!("{}{}", base_url, request.url())).unwrap();
+      let mut code = String::new();
+      for (key, value) in url.query_pairs() {
+        if key == "code" {
+          code = value.into_owned();
+          break;
+        }
+      }
+      let response = config
+        .cluster_api_client()
+        .expect("Client connection failed")
+        .get(format!("{}/auth/github/cli", config.api_base_url))
+        .query(&[("code", code)])
+        .bearer_auth(config.bearer_token())
+        .send()
+        .unwrap();
+      let status_code = response.status();
+      if status_code.is_success() {
+        let session = response.json::<SessionCredentials>().unwrap();
+        config.store_token_from_session(&session).unwrap();
+        request
+          .respond(Response::empty(302).with_header(
+            Header::from_bytes(&b"Location"[..], &b"https://dosei.ai/login/cli"[..]).unwrap(),
+          ))
+          .unwrap();
+        return println!("Login Succeeded!");
+      }
+    }
+    let error_message = "Login Failed!";
+    request
+      .respond(Response::from_string(error_message).with_status_code(400))
+      .unwrap();
+    eprintln!("{}", error_message);
+  }
 }
--- cli/src/config.rs
+++ cli/src/config.rs
@@ -11,7 +11,7 @@ use uuid::Uuid;
 
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 // TODO: Get this from the API directly
-pub const GITHUB_CLIENT_ID: &str = "Iv1.0d2388105db85287";
+pub const GITHUB_CLIENT_ID: &str = "Iv1.261badedf2d43fd5";
 
 #[derive(Debug)]
 pub struct Config {
