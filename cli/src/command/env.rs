92ac3a51f0b979ff323ec5dc319b4c27387528b1
Alw3ys <alw3ys@gmail.com>
2024-01-24 12:38:49 +0100

    feat(cli): list owner level envs

--- cli/src/command/env.rs
+++ cli/src/command/env.rs
@@ -1,12 +1,31 @@
 use crate::config::Config;
 use crate::session::get_session_user;
+use serde::{Deserialize, Serialize};
 
 pub fn list_env(config: &'static Config) {
   let user = match get_session_user(config) {
     Ok(user) => user,
     _ => panic!("Something went wrong"),
   };
-  println!("{:?}", user);
+  let response = config
+    .cluster_api_client()
+    .expect("Client connection failed")
+    .get(format!("{}/envs/{}", config.api_base_url, user.id))
+    .bearer_auth(config.bearer_token())
+    .send()
+    .unwrap();
+  if response.status().is_success() {
+    let result = response.json::<Vec<Env>>().unwrap();
+    for env in result {
+      println!("{}={}", env.name, env.value);
+    }
+  }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Env {
+  name: String,
+  value: String,
 }
 
 pub fn set_env(config: &'static Config) {
