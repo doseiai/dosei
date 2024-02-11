5f55fe5dd5dac4a13c02c37254b41c95b90f2b2b
Alw3ys <alw3ys@gmail.com>
2024-02-11 19:24:27 +0100

    chore: .env.template -> .env.example

--- script/configure-compose.sh
+++ script/configure-compose.sh
@@ -9,7 +9,7 @@ echo "
 /____/\____/___/___/___/
 "
 if ! [ -f .env ]; then
-    cp .env.template .env
+    cp .env.example .env
 
     JWT_SECRET=$(openssl rand -hex 8)
     NEW_POSTGRES_PASSWORD=$(openssl rand -hex 8)
