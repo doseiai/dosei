c4100700daa957beccae7904ef3a6abe0deaac58
Alvaro Molina <alw3ys@gmail.com>
2024-02-12 00:05:34 +0100

    feat: move dosei run and dosei export to dctl

---
+++ dctl/src/command/export.rs
@@ -0,0 +1,40 @@
+use crate::command::find_and_print_dosei_config_extension;
+use pyo3::exceptions::PySystemExit;
+use pyo3::prelude::*;
+use std::path::Path;
+
+pub fn export() {
+  let path = Path::new(".");
+  if let Ok(extension) = find_and_print_dosei_config_extension(path) {
+    match extension.as_str() {
+      "py" => {
+        Python::with_gil(|py| {
+          let dosei_main = py.import("dosei_sdk.main").unwrap();
+          let result = dosei_main.call_method("export", (), None);
+          if let Err(e) = result {
+            if e.is_instance(py, py.get_type::<PySystemExit>()) && e.value(py).to_string() != "0" {
+              println!("An error occurred: {:?}", e);
+            }
+          }
+        });
+      }
+      "js" | "mjs" | "cjs" | ".ts" | "tsx" => {
+        use std::process::{Command, Stdio};
+        let node_command = r#"
+        (async () => {
+          const { export_config } = await import('@dosei/dosei');
+          await export_config();
+        })();"#;
+        let output = Command::new("node")
+          .arg("-e")
+          .arg(node_command)
+          .env("NODE_PATH", ".")
+          .stdout(Stdio::piped())
+          .stderr(Stdio::piped())
+          .output();
+        println!("{:?}", output);
+      }
+      _ => unreachable!(),
+    }
+  }
+}
---
+++ dctl/src/command/run.rs
@@ -0,0 +1,39 @@
+use crate::command::find_and_print_dosei_config_extension;
+use clap::{Arg, ArgMatches, Command};
+use pyo3::exceptions::PySystemExit;
+use pyo3::prelude::*;
+use std::path::Path;
+
+pub fn sub_command() -> Command {
+  Command::new("run")
+    .about("Execute a Dosei App")
+    .arg(Arg::new("function").index(1).required(false))
+}
+
+pub fn run(arg_matches: &ArgMatches) {
+  let function = arg_matches.get_one::<String>("function");
+  let args = if function.is_some() {
+    (function,)
+  } else {
+    (None,)
+  };
+  let path = Path::new(".");
+  if let Ok(extension) = find_and_print_dosei_config_extension(path) {
+    match extension.as_str() {
+      "py" => {
+        Python::with_gil(|py| {
+          let dosei_main = py.import("dosei_sdk.main").unwrap();
+          if let Err(e) = dosei_main.call_method("run", args, None) {
+            if e.is_instance(py, py.get_type::<PySystemExit>()) && e.value(py).to_string() != "0" {
+              println!("An error occurred: {:?}", e);
+            }
+          }
+        });
+      }
+      "js" | "mjs" | "cjs" | ".ts" | "tsx" => {
+        todo!()
+      }
+      _ => unreachable!(),
+    }
+  }
+}
