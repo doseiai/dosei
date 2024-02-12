0f5260db12b5258faa410545d6fd12dc228258ca
Alw3ys <alw3ys@gmail.com>
2024-02-12 10:06:09 +0100

    feat: remove py3 depedency

--- dctl/src/command/run.rs
+++ dctl/src/command/run.rs
@@ -1,8 +1,7 @@
 use crate::command::find_and_print_dosei_config_extension;
 use clap::{Arg, ArgMatches, Command};
-use pyo3::exceptions::PySystemExit;
-use pyo3::prelude::*;
 use std::path::Path;
+use std::process::Stdio;
 
 pub fn sub_command() -> Command {
   Command::new("run")
@@ -12,24 +11,22 @@ pub fn sub_command() -> Command {
 
 pub fn run(arg_matches: &ArgMatches) {
   let function = arg_matches.get_one::<String>("function");
-  let args = if function.is_some() {
-    (function,)
-  } else {
-    (None,)
-  };
   let path = Path::new(".");
   if let Ok(extension) = find_and_print_dosei_config_extension(path) {
     match extension.as_str() {
       "py" => {
-        pyo3::prepare_freethreaded_python();
-        Python::with_gil(|py| {
-          let dosei_main = py.import("dosei_sdk.main").unwrap();
-          if let Err(e) = dosei_main.call_method("run", args, None) {
-            if e.is_instance(py, py.get_type::<PySystemExit>()) && e.value(py).to_string() != "0" {
-              println!("An error occurred: {:?}", e);
-            }
-          }
-        });
+        let arg = match function {
+          Some(command) => format!("from dosei_sdk import main\nmain.run(\"{}\")", command),
+          None => "from dosei_sdk import main\nmain.run()".to_string()
+        };
+        if let Err(err) = std::process::Command::new("python3")
+          .arg("-c")
+          .arg(arg)
+          .stdout(Stdio::inherit())
+          .stderr(Stdio::inherit())
+          .output() {
+          eprintln!("{:?}", err);
+        };
       }
       "js" | "mjs" | "cjs" | ".ts" | "tsx" => {
         todo!()
